de262e803cd5ba7ae1a8ab45f4a1aff2ee7d996e
Vance-sama <67249759+VansRouges@users.noreply.github.com>
2024-06-06 02:24:00 +0100

    removed password

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,36 +1,39 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
-import { Module } from '@nestjs/common';
+import { Module, Logger } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { WalletAddressModule } from './wallet-address/wallet-address.module';
 import { User } from './users/user.entity';
 import { WalletAddress } from './wallet-address/wallet-address.entity';
 
+const logger = new Logger('AppModule');
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'Sputnik0',
-      database: 'test_db',
-      entities: [User, WalletAddress],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [User, WalletAddress],
+        synchronize: true,
+        logger: 'advanced-console',
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
     WalletAddressModule,
   ],
 })
-export class AppModule {}
-
+export class AppModule {
+  constructor() {
+    logger.log('Application is starting...');
+  }
+}
